1e5efbe2aef5a99d8e0dde7f24e81c204106c2e3
Goodness <goodnessabisoye@gmail.com>
2023-09-18 09:16:45 +0100

    Debugged

--- auth/auth.py
+++ auth/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
-from ..model import db, register, load_all
+from model import db, register, load_all, insert_all
 
 auth_bp = Blueprint("auth_bp", __name__,
                     template_folder="templates", url_prefix=('/auth'),
@@ -10,10 +10,23 @@ def login():
     if request.method == "POST":
         username = request.form['username']
         password = request.form['password']
-        login_details = register.query.all()
+        login_details = load_all()
+        error = None
+        print("<<<<<<<<<<<<<<DONE>>>>>>>>>>>>>>>" + username + "  " + password)
         for data in login_details:
-            print(data.name)
-            print(data.password)
+            n = data.name
+            p = data.password
+
+        if n != username:
+            error = "INVALID USERNAME"
+
+        elif p != password:
+            error = "INVALID PASSWORD"
+        
+        if error != None:
+            flash(error)
+
+        return redirect(url_for("index"))
     return render_template("login.html")
 
 @auth_bp.route("/register", methods=("GET", "POST"))
@@ -22,13 +35,9 @@ def register():
         username = request.form['username']
         password = request.form['password']
         secret_key = request.form['secret_key']
+        print(username + " " + password + " " + secret_key)
 
-        add_details = register(name=username, password=password, secret_key=secret_key)
-        try:
-            db.session.add(add_details)
-            db.session.commit()
-            return redirect(url_for("auth.login"))
-        except:
-            error = "USERNAME OR PASSWORD EXISTS"
-            flash(error)
+        add_details = [username, password, secret_key]
+        insert_all(add_details)
+        return redirect(url_for("auth_bp.login"))
     return render_template("register.html")
--- model.py
+++ model.py
@@ -12,3 +12,10 @@ class register(db.Model):
 def load_all():
     data = register.query.all()
     return data
+
+def insert_all(x):
+    print(x[0] + " " + x[1] + " " + x[2])
+    add_details = register(name=x[0], password=x[1], secret_key=x[2])
+    db.session.add(add_details)
+    db.session.commit()
+    print("-------- ADD SUCCESSFUL ----------")
