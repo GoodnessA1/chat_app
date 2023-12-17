bc171b370490412c600aaa91300bd5a7727b8798
Goodness <goodnessabisoye@gmail.com>
2023-12-18 00:14:25 +0100

    Mew

--- home/home.py
+++ home/home.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, redirect
 
 home_bp = Blueprint('home_bp', __name__,
-                    template_folder="templates", static_folder="static")
+                    template_folder="templates", static_folder="static",
+                    url_prefix="/test")
 
 @home_bp.route('/')
 def index():
